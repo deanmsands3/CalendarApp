0dfd252132bc956b1f8930378375c75c149bdfd1
Dean Sands <deanmsands3@gmail.com>
2020-07-10 09:29:05 -0500

    Update Category.py
    Keeping class functions as class functions. Extending them a bit too.

--- NewEvents/Category.py
+++ NewEvents/Category.py
@@ -1,22 +1,26 @@
 import json
 class Category:
+    categories=dict()
+    default_categories_file="categories.json"
 
     @classmethod
-    def createSuggestedCategory(self):
+    def createSuggestedCategory(cls):
         standardcategories = {
             "unnecessary": 0,
             "unimportant": 1,
             "important": 2,
             "crucial": 3,
             "fixed": 4
-            }
-        with open('categories.json','w') as f:
-            
-            json.dump(standardcategories,f,indent=2)
-
-
+        }        
+        cls.categories.update(standardcategories)
+        cls.write_categories_to_file()
+    
     @classmethod
-    def createOwnCategory(self, anotherCategory, categories):
-        categories[anotherCategory] = len(categories)
-        with open('categories.json','w',encoding='utf-8') as f:
-            json.dump(categories,f,indent=2)
+    def createOwnCategory(cls, anotherCategory, categories):
+        cls.categories[anotherCategory] = len(cls.categories)
+        cls.write_categories_to_file()
+        
+    @classmethod
+    def write_categories_to_file(file_name=default_categories_file)
+        with open(file_name,'w') as f:
+            json.dump(cls.categories,f,indent=2)
